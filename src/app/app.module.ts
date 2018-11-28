aa25a4b2156252b42589a1ff4873fcf55205d94e
xXpilintuXx <ema-nuel06@hotmail.com>
2018-11-28 17:30:19 -0600

    Fin seccion 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { PortitemComponent } from './pages/portitem/portitem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    PortitemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/portitem/portitem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portitem',
+  templateUrl: './portitem.component.html',
+  styleUrls: ['./portitem.component.css']
+})
+export class PortitemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
